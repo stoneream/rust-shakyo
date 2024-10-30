2544d85b406de7078c6a9ac5e1f4c76f1abf22fa
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-10-31 02:25:19 +0900

    chapter12 wip

--- chapter12/src/main.rs
+++ chapter12/src/main.rs
@@ -1,25 +1,22 @@
+extern crate chapter12;
+
 use std::{env, process};
-use std::fs::File;
-use std::io::Read;
+
+use chapter12::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
-        eprintln!("not enough arguments");
-        println!("Usage: {} <query> <filename>", args[0]);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = chapter12::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
-
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-    println!("with text:\n{}", contents);
 }
