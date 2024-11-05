50f796f47bd158be34e61e0084c9d70a562a2747
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-11-05 23:06:43 +0900

    12.4 章

--- chapter12/src/main.rs
+++ chapter12/src/main.rs
@@ -12,9 +12,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = chapter12::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
