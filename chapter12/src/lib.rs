9f93da33a7d90859c3202e21b695ceb00a398917
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-11-16 05:27:36 +0900

    chapter 13 のいろいろ

--- chapter12/src/lib.rs
+++ chapter12/src/lib.rs
@@ -10,13 +10,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, String> {
-        if args.len() < 3 {
-            return Err(format!("Usage: {} <query> <filename>", args[0]));
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            // クエリ文字列を取得しませんでした
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            // ファイル名を取得しませんでした
+            None => return Err("Didn't get a file name"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -44,15 +51,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 
--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -1,6 +1,35 @@
 use std::{thread};
 use std::time::Duration;
 
+struct Cacher<T>
+where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
@@ -8,18 +37,25 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
 
         println!(
             // 今日は{}回腕立て伏せをしてください！
             "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
 
         println!(
             // 次に、{}回腹筋をしてください！
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -29,7 +65,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!(
                 // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
@@ -43,4 +79,16 @@ fn main() {
         simulated_user_specified_value,
         simulated_random_number
     );
+
+    let v1 = vec![1,2,3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
 }
