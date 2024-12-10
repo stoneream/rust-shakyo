85b19207224ed263d5ab449e3acc1ebc1bf04f9e
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-12-11 00:29:28 +0900

    Arc, Mutex

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -62,4 +62,23 @@ fn main() {
     for received in rx {
         println!("Got: {}", received);
     }
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
