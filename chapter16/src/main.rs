18f395a6dd9365ec6dd80dc4983b4e5d39a7fd78
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-12-11 00:10:52 +0900

    spawn

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -5,25 +5,16 @@ fn main() {
 
     let handle = thread::spawn(|| {
         for i in 1..10 {
-            println!("hi number {} from the spawned thread!!", i);
+            println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    // スレッドをブロックして終了を待つ
     handle.join().unwrap();
 
     for i in 1..5 {
         println!("hi number {} from the main thread!!", i);
         thread::sleep(Duration::from_millis(1));
     }
-
-    // handle.join().unwrap();
-
-    let v = vec![1, 2, 3];
-
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
-    });
-    
-    handle.join().unwrap();
 }
