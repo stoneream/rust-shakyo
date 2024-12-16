08cd069122c710a148a93e5c5433772378f443d1
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-12-17 00:37:18 +0900

    chapter17

---
+++ chapter17/src/main.rs
@@ -0,0 +1,52 @@
+use chapter17::{Screen, Draw, Post, Button};
+
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        //セレクトボックスを実際に描画するコード
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    // はい
+                    String::from("Yes"),
+                    // 多分
+                    String::from("Maybe"),
+                    // いいえ
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button{
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
