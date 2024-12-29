703ee3b9f9230fdfbb45ce1e7b6465eced2a32b5
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-12-29 10:56:13 +0900

    shutdown

--- chapter20/src/main.rs
+++ chapter20/src/main.rs
@@ -10,13 +10,16 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    // リクエストを2回受け付けたあとにショットダウン
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
